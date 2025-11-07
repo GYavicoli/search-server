04e65b1fec5760594ef3e4b5afbe33092bf6748e
GYavicoli <gyav@Garys-MacBook-Pro.local>
2025-11-07 08:27:00 -0700

    cleanup

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5001;
+const port = process.env.SERVER_PORT || 5001;
 const service = require('./Service.js');
 const instance = new service();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173' // Allow requests from your frontend
+    origin: process.env.CLIENT || 'http://localhost:5173'
 }));
 
 app.post('/api/search', async (req, res) => {
