e66d564f861cf56eff8485af53d8f3b4dc35281e
I_dodge_bullets <117242335+GYavicoli@users.noreply.github.com>
2025-11-07 21:06:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.post('/api/search', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on:${port}`);
 });
